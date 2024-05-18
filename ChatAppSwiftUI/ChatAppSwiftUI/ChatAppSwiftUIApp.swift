883e1b6485668eb5fc6adb7509fce4dbdceb2d91
phantamnhu <nhuphan160200@gmail.com>
2024-05-18 23:29:45 +0700

    setup project structure & add tabbar view

---
+++ ChatAppSwiftUI/ChatAppSwiftUI/ChatAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSwiftUIApp.swift
+//  ChatAppSwiftUI
+//
+//  Created by Phan Tam Nhu on 18/5/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
